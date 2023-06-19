1cae3849f6582dd0670cd90f3eccf5ce3cb6e401
browsfer <126813633+browsfer@users.noreply.github.com>
2023-06-19 20:22:36 +0200

    showing current users

--- lib/widgets/advanced_drawer.dart
+++ lib/widgets/advanced_drawer.dart
@@ -2,7 +2,6 @@ import 'package:chatify/screens/user_settings_screen.dart';
 import 'package:chatify/widgets/get_usernames.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 
@@ -29,8 +28,14 @@ class _MyAdvancedDrawerState extends State<MyAdvancedDrawer> {
         .collection('users')
         .get()
         .then((snapshot) => snapshot.docs.forEach((element) {
-              docIDs.add(element.reference.id);
+              docIDs.insert(0, element.reference.id);
             }));
+  }
+
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
   }
 
   @override
@@ -138,23 +143,32 @@ class _MyAdvancedDrawerState extends State<MyAdvancedDrawer> {
                       leading: const Icon(Icons.settings),
                       title: const Text('Settings'),
                     ),
+                    Spacer(),
 
                     // L O G G E D  U S E R S
                     //have to finish this one
-                    FutureBuilder(
-                      future: getUsers(),
-                      builder: (context, snapshot) {
-                        return Expanded(
-                          child: ListView.builder(
-                            itemCount: docIDs.length,
-                            itemBuilder: (context, index) => ListTile(
-                              title: GetUsernames(documentId: docIDs[index]),
+                    Card(
+                      color: Theme.of(context).colorScheme.secondary,
+                      child: Column(
+                        children: [
+                          const ListTile(
+                            leading: Icon(Icons.people),
+                            title: Text('Current users:'),
+                          ),
+                          Container(
+                            height: 170,
+                            child: ListView.builder(
+                              itemCount: docIDs.length,
+                              itemBuilder: (context, index) => ListTile(
+                                title: GetUsernames(documentId: docIDs[index]),
+                              ),
                             ),
                           ),
-                        );
-                      },
+                        ],
+                      ),
                     ),
-                    const Spacer(),
+
+                    // const Spacer(),
                     ListTile(
                       onTap: () => FirebaseAuth.instance.signOut(),
                       leading: const Icon(Icons.logout),
